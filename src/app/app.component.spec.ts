f2aa8e69e61baf2ed6becd34b968172c5c13041b
aaron7706 <78719495+aaron7706@users.noreply.github.com>
2023-10-02 11:14:02 +0530

    cgnages

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sirplay_task'`, () => {
+  it(`should have as title 'vrnl'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sirplay_task');
+    expect(app.title).toEqual('vrnl');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('sirplay_task app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('vrnl app is running!');
   });
 });
