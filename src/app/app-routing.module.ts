9dae5aa463c4a2cbc804afcc02db5d0d6c2263bc
Shwetayadav123 <116827484+Shwetayadav123@users.noreply.github.com>
2023-10-03 16:53:53 +0530

    ch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainSecComponent } from './main-sec/main-sec.component';
-import { DemoComponent } from './demo/demo.component';
 import { OnlineSportsbookComponent } from './online-sportsbook/online-sportsbook.component';
 import { OnlineCasinoSoftwareComponent } from './online-casino-software/online-casino-software.component';
 import { LiveCasinoComponent } from './live-casino/live-casino.component';
@@ -11,19 +10,11 @@ import { CasinoTournamentComponent } from './casino-tournament/casino-tournament
 import { StreamingComponent } from './streaming/streaming.component';
 import { WhitelabelComponent } from './whitelabel/whitelabel.component';
 import { WhitecasinoComponent } from './whitecasino/whitecasino.component';
-import { Web3sportbookComponent } from './web3sportbook/web3sportbook.component';
-import { CryptoComponent } from './crypto/crypto.component';
-import { Web3casinoComponent } from './web3casino/web3casino.component';
-import { DecentralizedComponent } from './decentralized/decentralized.component';
-import { BitcoinComponent } from './bitcoin/bitcoin.component';
-import { CertificateComponent } from './certificate/certificate.component';
 import { ContactComponent } from './contact/contact.component';
 import { WageringComponent } from './wagering/wagering.component';
-import { CashoutComponent } from './cashout/cashout.component';
 import { IFrameComponent } from './i-frame/i-frame.component';
-import { TerminalComponent } from './terminal/terminal.component';
+
 import { SportbetComponent } from './sportbet/sportbet.component';
-import { RetailComponent } from './retail/retail.component';
 import { EsportComponent } from './esport/esport.component';
 import { MobileComponent } from './mobile/mobile.component';
 import { AboutComponent } from './about/about.component';
@@ -34,7 +25,6 @@ import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.componen
 
 const routes: Routes = [
   {path:'',component:MainSecComponent},
-  {path:'demo-sportsbook',component:DemoComponent},
   {path:'online-sportsbook-software',component:OnlineSportsbookComponent},
   {path:'online-casino-software',component:OnlineCasinoSoftwareComponent},
   {path:'casino/live-casino',component:LiveCasinoComponent},
@@ -44,19 +34,13 @@ const routes: Routes = [
   {path:'streaming-platform-casino-and-betting',component:StreamingComponent},
   {path:'white-label-betting-platform',component:WhitelabelComponent},
   {path:'casino',component:WhitecasinoComponent},
-  {path:'web3-sportsbook',component:Web3sportbookComponent},
-  {path:'crypto-casino',component:CryptoComponent},
-  {path:'web3-casino',component:Web3casinoComponent},
-  {path:'decentralized-sports-betting', component:DecentralizedComponent},
-  {path:'bitcoin-sportsbook',component:BitcoinComponent},
-  {path:'certified-sports-betting-software',component:CertificateComponent},
+
   {path:'contact',component:ContactComponent},
   {path:'wagering-solutions',component:WageringComponent},
-  {path:'cashout', component:CashoutComponent},
   {path:'sportsbook-iframe', component:IFrameComponent},
-  {path:'betting-terminals', component:TerminalComponent},
+
   {path:'sports-betting-software', component:SportbetComponent},
-  {path:'sports-betting-software/retail-solutions',component:RetailComponent},
+
   {path:'sports-betting-software/esports-betting-software',component:EsportComponent},
   {path:'sports-betting-software/mobile-betting-software',component:MobileComponent},
   {path:'about-us',component:AboutComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { MainSecComponent } from './main-sec/main-sec.component';
 import { FooterComponent } from './footer/footer.component';
-import { DemoComponent } from './demo/demo.component';
 import { OnlineSportsbookComponent } from './online-sportsbook/online-sportsbook.component';
 import { OnlineCasinoSoftwareComponent } from './online-casino-software/online-casino-software.component';
 import { Header2Component } from './header2/header2.component';
@@ -19,20 +18,14 @@ import { CasinoTournamentComponent } from './casino-tournament/casino-tournament
 import { StreamingComponent } from './streaming/streaming.component';
 import { WhitelabelComponent } from './whitelabel/whitelabel.component';
 import { WhitecasinoComponent } from './whitecasino/whitecasino.component';
-import { Web3sportbookComponent } from './web3sportbook/web3sportbook.component';
-import { CryptoComponent } from './crypto/crypto.component';
-import { Web3casinoComponent } from './web3casino/web3casino.component';
-import { DecentralizedComponent } from './decentralized/decentralized.component';
-import { BitcoinComponent } from './bitcoin/bitcoin.component';
-import { CertificateComponent } from './certificate/certificate.component';
 import { ContactComponent } from './contact/contact.component';
 import { WageringComponent } from './wagering/wagering.component';
-import { CashoutComponent } from './cashout/cashout.component';
+
 import { IFrameComponent } from './i-frame/i-frame.component';
-import { TerminalComponent } from './terminal/terminal.component';
+
 import { Header3Component } from './header3/header3.component';
 import { SportbetComponent } from './sportbet/sportbet.component';
-import { RetailComponent } from './retail/retail.component';
+
 import { EsportComponent } from './esport/esport.component';
 import { MobileComponent } from './mobile/mobile.component';
 import { AboutComponent } from './about/about.component';
@@ -52,7 +45,6 @@ import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.componen
     HeaderComponent,
     MainSecComponent,
     FooterComponent,
-    DemoComponent,
     OnlineSportsbookComponent,
     OnlineCasinoSoftwareComponent,
     Header2Component,
@@ -64,20 +56,14 @@ import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.componen
     StreamingComponent,
     WhitelabelComponent,
     WhitecasinoComponent,
-    Web3sportbookComponent,
-    CryptoComponent,
-    Web3casinoComponent,
-    DecentralizedComponent,
-    BitcoinComponent,
-    CertificateComponent,
     ContactComponent,
     WageringComponent,
-    CashoutComponent,
+ 
     IFrameComponent,
-    TerminalComponent,
+
     Header3Component,
     SportbetComponent,
-    RetailComponent,
+ 
     EsportComponent,
     MobileComponent,
     AboutComponent,
