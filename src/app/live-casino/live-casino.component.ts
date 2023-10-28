dce0a1b8a23f4777a66708f5d8f28d6ae593541f
Shruti865 <116834715+Shruti865@users.noreply.github.com>
2023-10-28 12:34:10 +0530

    changes

--- src/app/live-casino/live-casino.component.ts
+++ src/app/live-casino/live-casino.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-live-casino',
@@ -7,8 +7,11 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./live-casino.component.css']
 })
 export class LiveCasinoComponent {
-  constructor(private titleService: Title){}
-  ngOnInit() {
-    this.titleService.setTitle('Live Casino - White label Casino');
+
+  constructor(private titleService: Title,
+    private meta: Meta) {
+      this.titleService.setTitle('Live Casino - White label Casino');
+      this.meta.addTag({name:'description',content:'Immerse yourself in a variety of live casino games for an exciting gaming experience.'});
+      this.meta.addTag({name:'keyword',content:'live casino, casino'});
   }
 }
