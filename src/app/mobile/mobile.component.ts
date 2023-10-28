88acffd6e676918d082ce96030ff2dd643253f4c
Shwetayadav123 <116827484+Shwetayadav123@users.noreply.github.com>
2023-10-28 14:15:09 +0530

    ch

--- src/app/mobile/mobile.component.ts
+++ src/app/mobile/mobile.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-mobile',
@@ -7,9 +7,10 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./mobile.component.css']
 })
 export class MobileComponent {
-  constructor(private titleService: Title) {
-  }
-  ngOnInit() {
-    this.titleService.setTitle('Mobile Sports Betting Software: Enhancing the Betting Experience');
+  constructor(private titleService: Title,
+    private meta: Meta) {
+      this.titleService.setTitle("Uplift Your Game with VRNL's Mobile Sports Betting Software");
+      this.meta.addTag({name:'description',content:"Experience the next level of sports betting with VRNL's Mobile Sports Betting Software. Enjoy seamless, real-time wagering on your favorite sports, whether you're a seasoned bettor or a newcomer."});
+      this.meta.addTag({name:'keyword',content:'Mobile sports betting, Sports betting , White label casino'});
   }
 }
