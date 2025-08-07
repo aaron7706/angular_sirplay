78225dcb53114c1e9a916ad8ef285fb961cc954e
Arvind Kunwar <109291011+Arvin-k17@users.noreply.github.com>
2025-08-07 18:46:06 +0530

    change

--- src/app/our-theme/our-theme.component.ts
+++ src/app/our-theme/our-theme.component.ts
@@ -7,24 +7,35 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./our-theme.component.css']
 })
 export class OurThemeComponent {
-  copyed: string | null = null;
+  selectedDemoUrl: string = '';
 
-  copytoclip(value: string) {
-    if (this.copyed !== value) {
-      const textToCopy = document.getElementById(value)?.textContent || '';
-      navigator.clipboard.writeText(textToCopy).then(() => {
-        this.copyed = value;
-  
-        setTimeout(() => {
-          this.copyed = null;
-        }, 3000);
-      });
-    }
+  cards = [
+    { name: 'Skyexch', img: 'assets/skylogo.png', url: 'https://skyexch.vrnlexch.com/' },
+    { name: 'Velki', img: 'assets/velkilogo.webp', url: 'https://velki.vrnlexch.com/' },
+    { name: 'Babu365', img: 'assets/Babu365.png', url: 'https://babu365.vrnlexch.com/#/' },
+    { name: 'Cxwelcome', img: 'assets/cxlogo.webp', url: 'https://www.cxwelcome.live' },
+    { name: 'mostvipgame', img: 'assets/mostvipgame-logo.webp', url: 'https://mostvipgame.vrnlexch.com/m/#/' },
+    { name: '9bazi', img: 'assets/logo-bazi.webp', url: 'https://9bazi.vrnlexch.com/' },
+    { name: 'Aura444', img: 'assets/aura.webp', url: 'https://aura.vrnlexch.com/' },
+    { name: 'Diamond', img: 'assets/diamond.png', url: 'http://www.diamond.vrnlexch.com/' },
+    { name: 'Lotus', img: 'assets/lotus.webp', url: 'https://lotus.vrnlexch.com/' },
+    { name: 'World777', img: 'assets/world7777.webp', url: 'https://world777.vrnlexch.com/' },
+    { name: 'Gullybet', img: 'assets/gullybet.webp', url: 'https://gullybet.vrnlexch.com/' }
+  ];
+
+  isDemoVisible = false;
+
+  openDemoModal(url: string): void {
+    this.selectedDemoUrl = url;
+    this.isDemoVisible = true;
+  }
+  closeDemoModal() {
+    this.isDemoVisible = false;
   }
   constructor(private titleService: Title) {
   }
   ngOnInit() {
     this.titleService.setTitle('Bet & Win: Our Work in the World of Online Betting and Casinos');
   }
-  
+
 }
